357c0f6f234359a2b46deb11a10b88b28b0e1c97
Katarzyna Jabłońska <147647574+katJablonska@users.noreply.github.com>
2024-01-18 14:04:41 +0100

    Add files via upload

---
+++ SK2-projekt/Client.cpp
@@ -0,0 +1,20 @@
+#include "Client.h"
+#include "Server.h"
+
+Client::Client(int fd) : fd(fd) {}
+
+void Client::addSub(int channelId) {
+    subscriptions.insert(channelId);
+}
+
+void Client::removeSub(int channelId) {
+    subscriptions.erase(channelId);
+}
+
+const std::set<int>& Client::getSubs() const {
+    return subscriptions;
+}
+
+bool Client::hasSubscribed(int channelId) const {
+    return subscriptions.find(channelId) != subscriptions.end();
+}
